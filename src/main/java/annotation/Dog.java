984e10f18e865ebb57d2166af9fc335dfd8b23c5
origin <git@github.com:OljaKn/job4j_design.git>
2024-04-01 14:18:11 +0300

    работа со Spring

---
+++ src/main/java/annotation/Dog.java
@@ -0,0 +1,20 @@
+package annotation;
+
+public class Dog implements Pet<Dog> {
+    private String name = "Dog";
+    public Dog() {
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Dog(String name) {
+        this.name = name;
+    }
+    @Override
+    public String print() {
+        return "Dog name = %s".formatted(name);
+    }
+}
